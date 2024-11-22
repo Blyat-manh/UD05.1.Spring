3e8920d46bffe98068dc44dab0fcfd4a3c207202
Blyat-manh <152481587+Blyat-manh@users.noreply.github.com>
2024-11-22 11:57:21 +0100

    Delete src directory

--- src/main/java/com/aplicacionnominas/empresa/repository/EmpleadoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aplicacionnominas.empresa.repository;
-
-import com.aplicacionnominas.empresa.model.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpleadoRepository extends JpaRepository<Empleado, String> {
-}
